b0ab4ded63f8c6eec4b1ac03744daf499fa8d794
Devi Dutt <devidutt2022@gmail.com>
2026-08-27 00:45:16 +0530

    Initial commit

---
+++ 02_basic_calculator.r
@@ -0,0 +1,15 @@
+# 02_basic_calculator.r
+# Basic Calculator Program in R
+
+a <- 20
+b <- 10
+
+addition <- a + b
+subtraction <- a - b
+multiplication <- a * b
+division <- a / b
+
+print(paste("Addition:", addition))
+print(paste("Subtraction:", subtraction))
+print(paste("Multiplication:", multiplication))
+print(paste("Division:", division))
