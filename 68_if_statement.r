ddc12e7166cbd52a02be78b410cfb0d413d12344
Devi Dutt <devidutt2022@gmail.com>
2026-09-10 13:29:55 +0530

    New Program Added

---
+++ 68_if_statement.r
@@ -0,0 +1,17 @@
+# 68_if_statement.r
+# Program to demonstrate a simple if statement in R
+
+# Create a variable
+marks <- 75
+
+# Check the condition
+if (marks >= 40) {
+  print("Student has passed.")
+}
+
+# Check another condition
+if (marks < 40) {
+  print("Student has failed.")
+}
+
+# End of the program
