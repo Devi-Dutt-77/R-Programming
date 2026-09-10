76c4b1b32e2e976f4678792b50dfdc23eff3843f
Devi Dutt <devidutt2022@gmail.com>
2026-09-10 13:31:01 +0530

    New Program Added

---
+++ 69_if_else_statement.r
@@ -0,0 +1,14 @@
+# 69_if_else_statement.r
+# Program to demonstrate if-else statement in R
+
+# Create a variable
+marks <- 35
+
+# Check whether the student has passed or failed
+if (marks >= 40) {
+  print("Student has passed.")
+} else {
+  print("Student has failed.")
+}
+
+# End of the program
