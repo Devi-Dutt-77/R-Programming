3adabc7e61ca3824c8f1ec1772b40b3d569a748e
Devi Dutt <devidutt2022@gmail.com>
2026-09-11 07:38:26 +0530

    New Program Added

---
+++ 74_while_loop.r
@@ -0,0 +1,15 @@
+# 74_while_loop.r
+# Program to demonstrate while loop in R
+
+# Initialize a variable
+i <- 1
+
+# Execute the loop while the condition is TRUE
+while (i <= 10) {
+  print(i)
+  
+  # Increment the value
+  i <- i + 1
+}
+
+# End of the program
