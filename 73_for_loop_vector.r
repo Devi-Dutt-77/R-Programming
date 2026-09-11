2088f5652b7cb211f476c2acc0eb6f35043f9b31
Devi Dutt <devidutt2022@gmail.com>
2026-09-11 07:37:53 +0530

    New Program Added

---
+++ 73_for_loop_vector.r
@@ -0,0 +1,12 @@
+# 73_for_loop_vector.r
+# Program to demonstrate for loop with a vector in R
+
+# Create a vector of marks
+marks <- c(75, 82, 68, 90, 85)
+
+# Display each mark using for loop
+for (mark in marks) {
+  print(mark)
+}
+
+# End of the program
