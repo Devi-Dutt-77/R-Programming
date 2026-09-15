2eac1198fc92aa3db84be00da4ba7a763d65da6c
Devi Dutt <devidutt2022@gmail.com>
2026-09-15 09:20:05 +0530

    New Program Added

---
+++ 91_simple_function.r
@@ -0,0 +1,33 @@
+# 91_simple_function.r
+# Program to demonstrate simple functions in R
+
+
+# Create a simple function
+greet <- function() {
+  print("Hello! Welcome to R Programming")
+}
+
+# Call the function
+greet()
+
+
+# Create a function to display a message
+welcome_message <- function() {
+  print("Learning Statistical Analysis using R")
+}
+
+# Call the function
+welcome_message()
+
+
+# Create a simple function to display a number
+display_number <- function() {
+  number <- 100
+  print(paste("The number is:", number))
+}
+
+# Call the function
+display_number()
+
+
+# End of the program
